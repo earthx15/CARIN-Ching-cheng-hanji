b3cac8ad938874be7cc5a43f20b991b3b0c7ba4e
Nuttapong9911 <nuttapongboonsala@gmail.com>
2022-02-15 22:01:39 +0700

    simplify some codes add peek and consume method finish computeNext

--- Tokenizer.java
+++ Tokenizer.java
@@ -5,14 +5,13 @@ public class Tokenizer {
     private String next;
     private int pos;
 
-    private List<String> terminalSymbol = new ArrayList<>(Arrays.asList
-        (new String[]{ "antibody", "down", "downleft", "downright", "else"
-        , "if", "left", "move", "nearby", "right", "shoot", "then", "up"
-        , "upleft", "upright", "virus", "while"}));
+    private final List<String> terminalSymbol = new ArrayList<>(Arrays.asList
+        ("antibody", "down", "downleft", "downright", "else"
+                , "if", "left", "move", "nearby", "right", "shoot", "then", "up"
+                , "upleft", "upright", "virus", "while"));
 
-    private List<String> operators = new ArrayList<>(Arrays.asList
-        (new String[]{ "+", "-", "*", "/", "%"
-        , "^", "(", ")", "{", "}",}));
+    private final List<String> operators = new ArrayList<>(Arrays.asList
+        ("+", "-", "*", "/", "%", "^", "(", ")", "{", "}"));
         
 
     public Tokenizer(String src){
@@ -27,6 +26,15 @@ public class Tokenizer {
         while (pos < src.length() && isSpace(src.charAt(pos)))
             pos++;  // ignore whitespace
 
+        /**
+         * possible token
+         * 1. integer
+         * 2. operator, i.e. + - / * ( {
+         * 3. terminal symbol, i.e. down, if, while
+         * 4. identifier (ตัวแปร)
+         *
+         * */
+
         if(pos < src.length()){
             char c = src.charAt(pos);
 
@@ -40,9 +48,15 @@ public class Tokenizer {
                 pos++;
                 
             }else{
-                
-            }
+                s.append(c);
+                for (pos++; pos < src.length() && !Character.toString(src.charAt(pos)).equals(" "); pos++)
+                    s.append(src.charAt(pos));
 
+                if(!terminalSymbol.contains(s.toString())){    //need to check for identifier later
+                    // throw the token is neither terminalSymbol nor identifier
+                }
+            }
+            next = s.toString();
 
         }else
             next = "";
@@ -63,9 +77,17 @@ public class Tokenizer {
 
     private boolean isOperator(char c){
         String s = Character.toString(c);
-        if(operators.contains(c))
-            return true;
-        else
-            return false;
+        return operators.contains(s);
     }
+
+    public String peek(){
+        StringBuilder n = new StringBuilder();
+        n.append(next);
+        return n.toString();
+    }
+
+    public void consume() {
+        computeNext();
+    }
+
 }
