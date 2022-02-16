065de19dd05c6473964b32fd5c95b681fde019a9
Nuttapong9911 <nuttapongboonsala@gmail.com>
2022-02-16 12:18:27 +0700

    implement IfStatement

--- src/Parser.java
+++ src/Parser.java
@@ -5,6 +5,29 @@ interface Statement{
     void eval();
 }
 
+class IfStatement implements Statement{
+    private Expression cause;
+    private Statement trueStatement;
+    private Statement falseStatement;
+
+    public IfStatement(Expression cause, Statement ts){
+        this.cause = cause;
+        this.trueStatement = ts;
+    }
+
+    public IfStatement(Expression cause, Statement ts, Statement fs){
+        this(cause, ts);
+        this.falseStatement = fs;
+    }
+
+    @Override
+    public void eval() {
+        if(cause.eval() != 0)
+            trueStatement.eval();
+        else
+            falseStatement.eval();
+    }
+}
 
 
 class BlockStatement implements Statement{
