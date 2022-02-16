50d478f788e0df9c91592a1c202f0ad0492def12
Nuttapong9911 <nuttapongboonsala@gmail.com>
2022-02-16 12:15:57 +0700

    initially implement Statement and Expression, implement BlockStatement

--- src/Parser.java
+++ src/Parser.java
@@ -1,3 +1,36 @@
+import java.util.LinkedList;
+import java.util.List;
+
+interface Statement{
+    void eval();
+}
+
+
+
+class BlockStatement implements Statement{
+    private List<Statement> list;
+
+    public BlockStatement(List<Statement> l){
+        this.list = l;
+    }
+
+    @Override
+    public void eval(){
+        for(Statement s : list)
+            s.eval();
+    }
+}
+
+
+class Expression{
+
+    int eval(){
+        return 0;
+    }
+}
+
+
+
 public class Parser {
     
 }
