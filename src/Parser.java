82f8ed5c0a554718da409f0d444dcf68a009a9ea
Nuttapong9911 <nuttapongboonsala@gmail.com>
2022-02-19 17:10:14 +0700

    implement IfStatement back temporarily

--- src/Parser.java
+++ src/Parser.java
@@ -92,6 +92,27 @@ class BlockStatement implements Statement{
     }
 }
 
+class IfStatement implements Statement {
+    private Expression expr;
+    private Statement trueStatement;
+    private Statement falseStatement;
+
+    public IfStatement(Expression expr, Statement ts, Statement fs) {
+        this.expr = expr;
+        this.trueStatement = ts;
+        this.falseStatement = fs;
+    }
+
+    @Override
+    public void eval(Map<String, Integer> strg) {
+        if (expr.eval(strg) != 0)
+            trueStatement.eval(strg);
+        else
+            falseStatement.eval(strg);
+    }
+}
+
+
 class AssignmentStatement implements Statement{
     private Identifier var;
     private Expression expr;
