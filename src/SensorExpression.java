44da6029f55c186ccbc63ff5eff00f7977b3467c
Nuttapong9911 <nuttapongboonsala@gmail.com>
2022-02-26 15:55:53 +0700

    add constructor to SensorExpression

--- src/SensorExpression.java
+++ src/SensorExpression.java
@@ -1,8 +1,14 @@
 import java.util.Map;
 
 public class SensorExpression implements Expression {
+    private int distance;
+
+    public SensorExpression(int dt) {
+        this.distance = dt;
+    }
+
     @Override
     public int eval(Map<String, Integer> strg) {
-        return 0;
+        return distance;
     }
 }
