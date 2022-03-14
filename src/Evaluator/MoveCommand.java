0537fbec81942ce28586b231371fbc60f5c10091
Nuttapong9911 <nuttapongboonsala@gmail.com>
2022-03-14 18:19:50 +0700

    change variable's name strg -> binding add host, cellfield to eval's Parameter
    implement evaluating of MoveCommand
    move to the given direction

--- src/Evaluator/MoveCommand.java
+++ src/Evaluator/MoveCommand.java
@@ -15,8 +15,37 @@ public class MoveCommand implements Statement {
     }
 
     @Override
-    public void eval(Map<String, Integer> strg) {
-        System.out.println("Move : " + direction.eval(strg));
-        testOut.append("Move : ").append(direction.eval(strg)).append("\n");
+    public void eval(Map<String, Integer> binding, Host unit, CellsField cf) throws EvalError {
+        System.out.println("Move : " + direction.eval(binding));
+        testOut.append("Move : ").append(direction.eval(binding)).append("\n");
+
+        if(unit == null) throw new EvalError("Missing Host unit");
+
+        int dst_i = unit.getPosition()[0];
+        int dst_j = unit.getPosition()[1];
+
+        if(direction.eval(binding).equals("up")){
+            dst_i -= 1;
+        }else if(direction.eval(binding).equals("upright")){
+            dst_i -= 1;
+            dst_j += 1;
+        }else if(direction.eval(binding).equals("right")){
+            dst_j += 1;
+        }else if(direction.eval(binding).equals("downright")){
+            dst_i += 1;
+            dst_j += 1;
+        }else if(direction.eval(binding).equals("down")){
+            dst_i += 1;
+        }else if(direction.eval(binding).equals("downleft")){
+            dst_i += 1;
+            dst_j -= 1;
+        }else if(direction.eval(binding).equals("left")){
+            dst_j -= 1;
+        }else if(direction.eval(binding).equals("upleft")){
+            dst_i -= 1;
+            dst_j -= 1;
+        }
+        if(cf.checkUnit(dst_i, dst_j).equals("empty") && !cf.checkUnit(dst_i, dst_j).equals("out"))
+            cf.moveUnit(dst_i, dst_j, unit);
     }
 }
