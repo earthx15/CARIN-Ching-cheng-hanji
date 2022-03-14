5a68021e6b588ddc76b08be0434b3d9e8767b74f
Nuttapong9911 <nuttapongboonsala@gmail.com>
2022-03-14 18:37:09 +0700

    add MainGame.java

---
+++ src/Game/MainGame.java
@@ -0,0 +1,22 @@
+package Game;
+
+import Entity.Virus;
+import Evaluator.Parser;
+
+import java.util.Random;
+
+public class MainGame {
+//    private int credits;
+//    private int timeUnits;
+//    private int timeSpeed;
+    protected static CellsField cellsField;
+
+    public static void main(String[] args){
+        Random rand = new Random();
+        System.out.println(rand.nextInt(100));
+        System.out.println(rand.nextInt(100));
+        System.out.println(rand.nextInt(100));
+
+    }
+
+}
